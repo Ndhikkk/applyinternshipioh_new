8afbbf317bf6b796b602db81936ad7eead452e84
vicky5571 <vickygp5571@gmail.com>
2026-08-18 11:16:34 +0700

    added change data type of status at pendaftaran magang sql file

--- database/tambahan3.sql
+++ database/tambahan3.sql
@@ -2,3 +2,6 @@
 -- Jalankan sekali di phpMyAdmin pada database aplikasi.
 ALTER TABLE `pendaftaran_magang`
   ADD COLUMN `kota_pilihan` VARCHAR(100) NULL AFTER `regional_interview`;
+
+ALTER TABLE `pendaftaran_magang`
+  MODIFY COLUMN `status` VARCHAR(50) NOT NULL DEFAULT 'Menunggu';
